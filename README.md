3023a16b0dc23cf9d7249bf499f72d0571ff9c25
Shadman Sahito <shadmansahitos@gmail.com>
2024-04-03 20:12:57 +0500

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple Number Guessing Game! created by Shadman Sahito."# Number-Guessing-Game-new" 
+"# Number-Guessing-Game" 
---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) Computer will generate a random number
+// 2) User input for guessing number
+// 3) Compare user input with computer generated number and show result
+
+const randomNumber= Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess the Number:",
+},
+]);
+if (answer.userGuessedNumber === randomNumber){
+    console.log("Congratulations! you guessed the right number.")
+}else{
+    console.log("You Guessed the wrong number");
+
+}
